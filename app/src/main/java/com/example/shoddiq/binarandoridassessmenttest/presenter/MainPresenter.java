cbcf8e0fedb7b648eb0ae5eb7cefa29c4645394c
shoddiqjati <shoddiq.jati@gmail.com>
2017-10-14 00:35:42 +0700

    Add AddActivity and MainPresenter

---
+++ app/src/main/java/com/example/shoddiq/binarandoridassessmenttest/presenter/MainPresenter.java
@@ -0,0 +1,7 @@
+package com.example.shoddiq.binarandoridassessmenttest.presenter;
+
+import com.example.shoddiq.binarandoridassessmenttest.view.ifaces.iMainView;
+
+public class MainPresenter {
+    private iMainView mView;
+}
--- app/src/main/java/com/example/shoddiq/binarandoridassessmenttest/view/MainActivity.java
+++ app/src/main/java/com/example/shoddiq/binarandoridassessmenttest/view/MainActivity.java
@@ -1,26 +1,74 @@
 package com.example.shoddiq.binarandoridassessmenttest.view;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.example.shoddiq.binarandoridassessmenttest.R;
+import com.example.shoddiq.binarandoridassessmenttest.model.Stuff;
+import com.example.shoddiq.binarandoridassessmenttest.view.ifaces.iMainView;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements iMainView {
 
     @BindView(R.id.act_main_recycler_view)
     RecyclerView recyclerView;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
     @BindView(R.id.fab)
     FloatingActionButton fab;
+
+    @OnClick(R.id.fab)
+    public void fabHandler() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+    }
+
+    @Override
+    public void displayData(List<Stuff> stuffList) {
+
+    }
+
+    @Override
+    public void setupView() {
+
+    }
+
+    @Override
+    public void setupListener() {
+
+    }
+
+    @Override
+    public void showLoading(boolean isLoading) {
+        if (isLoading) {
+            progressBar.setVisibility(View.VISIBLE);
+            recyclerView.setVisibility(View.GONE);
+            fab.setVisibility(View.GONE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            recyclerView.setVisibility(View.VISIBLE);
+            fab.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onItemTouch(int position) {
+
     }
 }
